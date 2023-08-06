90716bcbee08bd819b124eb04e8598e29e98ae5c
sanjay.shahi <stha.binod1000@gmail.com>
2023-08-06 08:36:35 +0545

    Added models

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,7 +4,16 @@ from django.db import models
         # column - title(length-100,string),
         # description(length-unlimited,string), status(bool)
         
-
+class Todo(models.Model):
+    # class variable
+    # char = string
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.BooleanField()
+    
+    # Workout
+    # I will be doing 2 hours of workout for a week
+    # false
     
     
     
