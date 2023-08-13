b317bfc55a217328b2551d40c5c4debda8ba2bed
sanjay.shahi <stha.binod1000@gmail.com>
2023-08-13 08:30:41 +0545

    template

--- demo_project_django/settings.py
+++ demo_project_django/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = "demo_project_django.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME':'todo_aug_11',
+       'USER': 'todo_user_aug_11',
+       'PASSWORD': 'password',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Todo 
 
 def list_todo(request):
     # logic
-    return render(request,"todo/index.html")
+    # select * from todo_app_todo;
+    # ORM - object relational mapper
+    # list
+    
+    todo_list = Todo.objects.all() 
+    print(todo_list)
+    data = {
+        'todo_list_data':todo_list
+    }
+    return render(request,"todo/index.html",data)    
