498dc3aa9c04f35d88bce6abd0b0a9367fb0680a
sanjay.shahi <stha.binod1000@gmail.com>
2023-08-15 08:35:52 +0545

    create

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_todo
+from .views import list_todo,create_todo
 
 urlpatterns = [
-    path("list",list_todo,name="my_todo_list")
+    path("list",list_todo,name="my_todo_list"),
+    path("create-todo",create_todo,name="create_my_todo")
   
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -2,16 +2,29 @@ from django.shortcuts import render
 from .models import Todo 
 
 def list_todo(request):
-    # logic
-    
+
     # SQL query
-    # select * from todo where id=1;
+    #select * from table_name;
     
-    # ORM query
+    # ORM Query
     todo_list = Todo.objects.all()
     
     data = {
-        'todo_list_all':todo_list
+        'todo_list_show':todo_list
     }
-   
+    
     return render(request,"todo/index.html",data)    
+
+
+def create_todo(request):
+    
+    if request.method=="POST":
+        # data - title description
+        data_title = request.POST['my_title']
+        data_description = request.POST['my_description']
+        print(data_title,data_description)
+        
+        # which table - Todo
+        Todo.objects.create(title=data_title,description=data_description)
+    
+    return render(request,"todo/create_todo.html")
