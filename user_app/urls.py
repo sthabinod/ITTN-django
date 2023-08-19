7070ca7e0afcc81d4902da9e0d985366b13c59cc
sanjay.shahi <stha.binod1000@gmail.com>
2023-08-19 16:26:03 +0545

    User registration

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import login_user
+from .views import login_user, register
 
 urlpatterns = [
-  path('login',login_user,name="login_user")
+  path('login',login_user,name="login_user"),
+  path('register',register,name="register"),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -15,11 +15,43 @@ def login_user(request):
         # Checking if the user exists in database
         if User.objects.filter(username=username).exists():
         #    login here
-            user = User.objects.get(username=username)
-            login(request, user)
+            user_obj = User.objects.get(username=username)
+            login(request, user_obj)
             return redirect('my_todo_list')
         else:
             messages.error(request, 'Username or password does not matched!')
     else:
         print("This is not POST method")
     return render(request, 'login.html')
+
+def register(request):
+    
+    
+    
+    # check if the user already exists
+        # already exists
+    
+    # if not
+        # create user
+        # send message
+    
+    # Check request method
+    if request.method == 'POST':
+        # get frontend username and password
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+
+        # Checking if the user exists in database
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'User already exists!')
+        else:
+            User.objects.create(username=username,password=password)
+            # user create
+            messages.info(request, 'User created Successfully!')
+            
+    else:
+        print("This is not POST method")
+    
+    
+    return render(request,"registration.html")
